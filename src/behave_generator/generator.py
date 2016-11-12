7698c25d72a88fdf89ee2dfc41a352ebba77549a
accraze <accraze@gmail.com>
2016-11-11 16:52:20 -0800

    feat(generator): create environment file

--- src/behave_generator/generator.py
+++ src/behave_generator/generator.py
@@ -1,5 +1,9 @@
+import os
 
 class BehaveGenerator(object):
+
+  environ_template = 'templates/environment_base.py'
+  environ_file = 'features/environment.py'
 
   def build(self):
     """
@@ -7,13 +11,27 @@ class BehaveGenerator(object):
     writing behave tests.
     """
     self._scaffold_dirs()
+    self._create_env_file()
 
   def _scaffold_dirs(self):
     """
-    Build feature and feature/steps dirs
+    Builds feature and feature/steps dirs
     which are required to run behave tests.
     """
     from subprocess import call
     call(["mkdir", "features"])
     call(["mkdir", "features/steps"])
 
+  def _create_env_file(self):
+    """
+    Create an environment.py file
+    and place it inside of feature dir
+    """
+    # with open("templates/environment_base.py") as env_template:
+    #   with open("features/environment.py", 'w') as env_file:
+    #     for line in env_template:
+    #       env_file.write(line)
+    this_dir, this_filename = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, self.environ_template)
+    from subprocess import call
+    call(["cp", DATA_PATH, self.environ_file])
---
+++ src/behave_generator/templates/environment_base.py
@@ -0,0 +1,6 @@
+
+def before_all():
+  pass
+
+def after_all():
+  pass
